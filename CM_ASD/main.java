7cc09db3f2b786fc1d13ac9a409032569ebb7924
Anggasaputraramadhan <anggasaputraramadhan5@gmail.com>
2025-06-03 20:45:43 +0700

    Add files via upload

--- CM_ASD/main.java
+++ CM_ASD/main.java
@@ -20,57 +20,21 @@ public class main {
 
             switch (pilihan) {
             case 1:
-                System.out.print("Plat Nomor: ");
-                String plat = input.nextLine();
-                System.out.print("Tipe Kendaraan: ");
-                String tipe = input.nextLine();
-                System.out.print("Merk Kendaraan: ");
-                String merk = input.nextLine();
-                Kendaraan kendaraan = new Kendaraan(plat, tipe, merk);
-
-                System.out.print("Nama BBM: ");
-                String namaBBM = input.nextLine();
-                System.out.print("Harga per Liter: ");
-                double harga = input.nextDouble();
-                input.nextLine();
-                BBM bbm = new BBM(namaBBM, harga);
-
-                System.out.print("Jumlah Liter: ");
-                double liter = input.nextDouble();
-                input.nextLine();
-
-                TransaksiPengisian transaksi = new TransaksiPengisian(kendaraan, bbm, liter);
-                antrian.tambahTransaksi(transaksi);
-                System.out.println("Kendaraan berhasil ditambahkan ke antrian.");
                 break;
 
             case 2:
-                System.out.println("\n=== Menampilkan Antrian ===");
-                antrian.tampilkanSemuaTransaksi();
                 break;
 
             case 3:
-                System.out.println("Cek Jumlah Antrian Kendaraan: " + antrian.hitungJumlah());
                 break;
 
             case 4:
-                TransaksiPengisian dilayani = antrian.layaniKendaraan();
-                if (dilayani != null) {
-                System.out.println("Kendaraan berikut telah dilayani:");
-                dilayani.tampilkanTransaksi();
-                riwayat.tambahTransaksi(dilayani);
-            } else {
-                System.out.println("Tidak ada kendaraan dalam antrian.");
-            }
                 break;
 
             case 5:
-                System.out.println("\n=== Menampilkan Riwayat Transaksi ===");
-                riwayat.tampilkanSemuaTransaksi();
                 break;
 
             case 0:
-                System.out.println("Terima kasih. Program selesai.");
                 break;
 
             default:
